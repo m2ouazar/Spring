a7c7db22f56a8e15de639c9f761d97e8ebc96aad
Mourad OUAZAR <m2ouazar@gmail.com>
2024-01-26 00:48:43 +0100

    Project creation

---
+++ Sport-QCM/src/main/java/com/m2ouazar/Sport/QCM/SportQcmApplication.java
@@ -0,0 +1,13 @@
+package com.m2ouazar.Sport.QCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportQcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportQcmApplication.class, args);
+	}
+
+}
---
+++ Sport-QCM/src/test/java/com/m2ouazar/Sport/QCM/SportQcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2ouazar.Sport.QCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportQcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
