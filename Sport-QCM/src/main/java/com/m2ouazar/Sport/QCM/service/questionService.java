add630814cf10529c965326170bf5a7aaacca2f7
Mourad OUAZAR <m2ouazar@gmail.com>
2024-01-28 16:03:44 +0100

    add random order for questions and answers

--- Sport-QCM/src/main/java/com/m2ouazar/Sport/QCM/service/questionService.java
+++ Sport-QCM/src/main/java/com/m2ouazar/Sport/QCM/service/questionService.java
@@ -5,27 +5,44 @@ import com.m2ouazar.Sport.QCM.repository.questionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class questionService {
 
-    private questionRepository questionRepository;
+    private final questionRepository questionRepository;
 
     @Autowired
     public questionService(questionRepository questionRepository) {
         this.questionRepository = questionRepository;
     }
 
-    //CRUD Operations
+    //CRUD OPERATIONS
+
     //GET ALL QUESTIONS
+    //RANDOM ORDER FOR QUESTIONS
+    //RANDOM ORDER FOR ANSWERS OF A QUESTION
     public List<question> getAllQuestions(){
-        return questionRepository.findAll();
+        List<question> randomList= new ArrayList<>(questionRepository.findAll());
+        randomList.forEach(a->{
+            List<String> opt=a.getOptions();
+            Collections.shuffle(opt);
+        });
+        Collections.shuffle(randomList);
+        return randomList;
     }
 
     //GET QUESTION BY ID
+    //RANDOM ORDER FOR ANSWERS OF A QUESTION
     public question questionById(String id){
-        return questionRepository.findById(id).orElse(null);
+        question question=questionRepository.findById(id).orElse(null);
+        if (question != null){
+            Collections.shuffle(question.getOptions());
+            return question;
+        }
+        return null;
     }
 
     //POST QUESTION
